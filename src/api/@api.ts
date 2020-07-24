d8fe09ee7ac6abb264abd7820129eb11bdb8b191
Dizy <mhisf@vip.qq.com>
2020-07-25 01:49:25 +0800

    Extend user api

--- src/api/@api.ts
+++ src/api/@api.ts
@@ -1,5 +1,10 @@
 import {LarkAPIClient} from '../client';
 
+export interface PageResponseData {
+  has_more: boolean;
+  page_token: string;
+}
+
 export abstract class LarkAPI {
   constructor(protected client: LarkAPIClient) {}
 }
--- src/api/contact/user.ts
+++ src/api/contact/user.ts
@@ -1,6 +1,24 @@
-import {LarkAPI} from '../@api';
+import {LarkAPI, PageResponseData} from '../@api';
 import {DataResponse} from '../../client';
 import QS from 'query-string';
+
+export interface ContactUserRefWithEmployeeIdOnly {
+  employee_id: string;
+}
+
+export interface ContactUserRefWithOpenIdOnly {
+  open_id: string;
+}
+
+export interface ContactUserRefWithBoth {
+  employee_id: string;
+  open_id: string;
+}
+
+export type ContactUserRef =
+  | ContactUserRefWithEmployeeIdOnly
+  | ContactUserRefWithOpenIdOnly
+  | ContactUserRefWithBoth;
 
 export enum EmployeeType {
   fulltime = 1,
@@ -67,9 +85,7 @@ export interface ContactDepartmentUserListOptions {
   fetch_child: boolean;
 }
 
-export interface ContactDepartmentUserListData {
-  has_more: boolean;
-  page_token: string;
+export interface ContactDepartmentUserListData extends PageResponseData {
   user_list: ContactUserBriefInfo[];
 }
 
@@ -81,14 +97,79 @@ export interface ContactDepartmentUserDetailListOptions {
   fetch_child: boolean;
 }
 
-export interface ContactDepartmentUserDetailListData {
-  has_more: boolean;
-  page_token: string;
+export interface ContactDepartmentUserDetailListData extends PageResponseData {
   user_list: ContactUserInfo[];
 }
 
+export interface ContactUserAddOptions<CustomAttrs = any> {
+  name: string;
+  email?: string;
+  mobile: string;
+  department_ids: string[];
+  mobile_visible?: boolean;
+  city?: string;
+  country?: string;
+  gender?: Gender;
+  employee_type?: EmployeeType;
+  join_time?: number;
+  leader_employee_id?: string;
+  leader_open_id?: string;
+  employee_id?: string;
+  employee_no?: string;
+  need_send_notification?: boolean;
+  custom_attrs?: CustomAttrs;
+  work_station?: string;
+}
+
+export interface ContactUserAddData {
+  user_info: ContactUserInfo;
+}
+
+export interface ContactUserDeleteOptions {
+  employee_id?: string;
+  open_id?: string;
+  department_chat_acceptor?: ContactUserRef;
+  external_chat_acceptor?: ContactUserRef;
+  docs_acceptor?: ContactUserRef;
+  calendar_acceptor?: ContactUserRef;
+  application_acceptor?: ContactUserRef;
+}
+
+export interface ContactUserUpdateOptionsWithEmployeeIdOnly
+  extends Partial<ContactUserInfo> {
+  employee_id: string;
+}
+
+export interface ContactUserUpdateOptionsWithOpenIdOnly
+  extends Partial<ContactUserInfo> {
+  open_id: string;
+}
+
+export type ContactUserUpdateOptions =
+  | ContactUserUpdateOptionsWithEmployeeIdOnly
+  | ContactUserUpdateOptionsWithOpenIdOnly;
+
+export interface ContactRole {
+  id: string;
+  name: string;
+}
+
+export interface ContactRoleListData {
+  role_list: ContactRole[];
+}
+
+export interface ContactUserEntry {
+  name: string;
+  open_id: string;
+  user_id: string;
+}
+
+export interface ContactRoleMembersData extends PageResponseData {
+  user_list: ContactUserEntry[];
+}
+
 export class ContactUserAPI extends LarkAPI {
-  async getUserInfos(idsType: 'open_ids' | 'employee_ids', ids: string[]) {
+  async getInfos(idsType: 'open_ids' | 'employee_ids', ids: string[]) {
     let tenant_access_token = await this.client.getTenantAccessToken();
 
     let query = ids.map(id => `${idsType}=${encodeURIComponent(id)}`).join('&');
@@ -124,5 +205,59 @@ export class ContactUserAPI extends LarkAPI {
     return await this.client.get<
       DataResponse<ContactDepartmentUserDetailListData>
     >(`contact/v1/department/user/detail/list?${query}`, tenant_access_token);
+  }
+
+  async add(options: ContactUserAddOptions) {
+    let tenant_access_token = await this.client.getTenantAccessToken();
+
+    return this.client.post<DataResponse<ContactUserAddData>>(
+      'contact/v1/user/add',
+      options,
+      tenant_access_token,
+    );
+  }
+
+  async delete(options: ContactUserDeleteOptions) {
+    let tenant_access_token = await this.client.getTenantAccessToken();
+
+    await this.client.post(
+      'contact/v1/user/delete',
+      options,
+      tenant_access_token,
+    );
+  }
+
+  async update(options: ContactUserUpdateOptions) {
+    let tenant_access_token = await this.client.getTenantAccessToken();
+
+    await this.client.post(
+      'contact/v1/user/update',
+      options,
+      tenant_access_token,
+    );
+  }
+
+  async getRoleList() {
+    let tenant_access_token = await this.client.getTenantAccessToken();
+
+    return this.client.get<DataResponse<ContactRoleListData>>(
+      'contact/v2/role/list',
+      tenant_access_token,
+    );
+  }
+
+  async getRoleMembers(roleId: string, pageSize = 20, pageToken?: string) {
+    let query = QS.stringify({
+      role_id: roleId,
+      page_size: pageSize,
+      page_token: pageToken,
+    });
+
+    let tenant_access_token = await this.client.getTenantAccessToken();
+
+    return this.client.get<DataResponse<ContactRoleMembersData>>(
+      `contact/v2/role/members?${query}`,
+      tenant_access_token,
+    );
   }
 }
